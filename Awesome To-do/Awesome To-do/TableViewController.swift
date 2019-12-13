5141528309096aef5300bf0a8612c8cd0cd8a681
Zachary Cruz <zscruzer@gmail.com>
2019-12-13 16:24:35 -0600

    I put in an empty almost empty table view.

---
+++ Awesome To-do/Awesome To-do/TableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Awesome To-do
+//
+//  Created by Zachary Cruz on 12/13/19.
+//  Copyright © 2019 zscruz. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        return cell
+    }
+
+}
+
