653461d9fa1816fc47a658858c578909bef33b45
Zachary Cruz <zscruzer@gmail.com>
2019-12-13 09:37:20 -0600

    Initial project creation. App should display the text 'Hello, World!'.

---
+++ Awesome To-do/Awesome To-do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Awesome To-do
+//
+//  Created by Zachary Cruz on 12/13/19.
+//  Copyright © 2019 zscruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
