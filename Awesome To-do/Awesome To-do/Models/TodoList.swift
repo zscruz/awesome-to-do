d1274240345081029f8b61dc138232321c4b8df7
Zachary Cruz <zscruzer@gmail.com>
2019-12-17 17:04:40 -0600

    Added TodoList model class. This is what the view controllers will operate off in the future.

---
+++ Awesome To-do/Awesome To-do/Models/TodoList.swift
@@ -0,0 +1,22 @@
+//
+//  TodoList.swift
+//  Awesome To-do
+//
+//  Created by Zachary Cruz on 12/17/19.
+//  Copyright © 2019 zscruz. All rights reserved.
+//
+
+import Foundation
+class TodoList {
+    var todoItems: [TodoItem] = []
+    
+    func addTodoItem(todoItemToAdd: TodoItem) {
+        todoItems.append(todoItemToAdd)
+    }
+    
+    func removeTodoItem(todoItemToRemove: TodoItem) {
+        if let index = todoItems.firstIndex(of: todoItemToRemove) {
+            todoItems.remove(at: index)
+        }
+    }
+}
